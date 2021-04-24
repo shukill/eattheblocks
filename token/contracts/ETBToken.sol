f58b6e5263bf052ec02e99ff4e680fde5a57d97d
shukill <56451088+shukill@users.noreply.github.com>
2021-04-25 01:11:48 +0530

    Update ETBToken.sol

--- token/contracts/ETBToken.sol
+++ token/contracts/ETBToken.sol
@@ -4,6 +4,6 @@ import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract ETBToken is ERC20 {
   constructor() ERC20('NFTCoin', 'NFT') {
-    _mint(msg.sender, 1000000 * 10 ** 18);
+    _mint(msg.sender, 10000 * 10 ** 18);
   }
 }
